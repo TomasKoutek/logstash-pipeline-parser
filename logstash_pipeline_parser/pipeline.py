422194a0e02dfb7e7e6b3a0654417dfb797b7c0e
Koutek, Tomas <tomas.koutek@dieboldnixdorf.com>
2023-12-15 15:59:42 +0100

    add Pipeline docstrings

--- logstash_pipeline_parser/pipeline.py
+++ logstash_pipeline_parser/pipeline.py
@@ -10,8 +10,16 @@ from .tree import AST
 
 
 class Pipeline:
+    """
+    A class representing the Logstash pipeline.
+    """
 
     def __init__(self, pipeline: str) -> NoReturn:
+        """
+        Instantiates class from a given string.
+
+        :param pipeline: a :py:class:`str`, the pipeline definition
+        """
         self._ast: AST = AST()
         self._data: str = pipeline
 
@@ -53,28 +61,249 @@ class Pipeline:
             .add_type("template_file", Path) \
             .add_type("truststore", Path)
 
-    def add_type(self, name: str, new_type: type[Any] | Callable[[Any], Any]) -> Self:
-        self._ast.add_type(name, new_type)
-        return self
-
-    def remove_type(self, name: str) -> Self:
-        self._ast.remove_type(name)
-        return self
-
-    def get_types(self) -> dict:
-        return self._ast.get_types()
-
     @classmethod
     def from_file(cls, path: str | Path) -> Self:
+        """
+        Instantiates class from a file.
+
+        :param path: :py:class:`str` or :py:class:`pathlib.Path`, the path to the file
+        :return: :py:class:`Pipeline`
+
+        For example from string:
+
+        .. code-block:: python
+
+           from logstash_parser import Pipeline
+
+           pipeline = Pipeline.from_file("/some/path/to/pipeline.conf")
+
+        Or from Path:
+
+        .. code-block:: python
+
+           from logstash_parser import Pipeline
+           from pathlib import Path
+
+           path = Path("/some/path/to/pipeline.conf")
+           pipeline = Pipeline.from_file(path)
+        """
         if isinstance(path, str):
             path = Path(path)
 
         return cls(path.read_text())
 
+    def add_type(self, name: str, new_type: type[Any] | Callable[[Any], Any]) -> Self:
+        """
+        Adds a new type
+
+        :param name: a :py:class:`str`, the type name
+        :param new_type: a type or callable, the new type for given name
+        :returns: :py:class:`Pipeline`
+
+        For example function:
+
+        .. code-block:: python
+
+           from logstash_parser import Pipeline
+
+           Pipeline("").add_type("port", str)
+
+        For example class:
+
+        .. code-block:: python
+
+           from logstash_parser import Pipeline
+
+           class MyPortType:
+               pass
+
+           Pipeline("").add_type("port", MyPortType)
+
+        .. note::
+
+           Please see :ref:`examples-type` for more examples.
+        """
+        self._ast.add_type(name, new_type)
+        return self
+
+    def remove_type(self, name: str) -> Self:
+        """
+        Removes a type
+        :param name: a :py:class:`str`, the type name
+        :return: :py:class:`Pipeline`
+
+        For example function:
+
+        .. code-block:: python
+
+           from logstash_parser import Pipeline
+
+           Pipeline("").remove_type("port")
+        """
+        self._ast.remove_type(name)
+        return self
+
+    def get_types(self) -> dict[str, type[Any] | Callable[[Any], Any]]:
+        """
+        Returns all defined types
+
+        :return: a :py:class:`dict`, all names as key and types as value
+
+        Predefined types are:
+
+        .. list-table::
+           :header-rows: 1
+           :widths: auto
+
+           * - Name
+             - Type/Callable
+           * - aggregate_maps_path
+             - :py:class:`pathlib.Path`
+           * - cacert
+             - :py:class:`pathlib.Path`
+           * - ca_file
+             - :py:class:`pathlib.Path`
+           * - client_cert
+             - :py:class:`pathlib.Path`
+           * - client_key
+             - :py:class:`pathlib.Path`
+           * - database
+             - :py:class:`pathlib.Path`
+           * - dictionary_path
+             - :py:class:`pathlib.Path`
+           * - jaas_path
+             - :py:class:`pathlib.Path`
+           * - jdbc_driver_library
+             - :py:class:`pathlib.Path`
+           * - jdbc_password_filepath
+             - :py:class:`pathlib.Path`
+           * - json_key_file
+             - :py:class:`pathlib.Path`
+           * - kerberos_config
+             - :py:class:`pathlib.Path`
+           * - keystore
+             - :py:class:`pathlib.Path`
+           * - mib_paths
+             - :py:class:`pathlib.Path`
+           * - network_path
+             - :py:class:`pathlib.Path`
+           * - path
+             - :py:class:`pathlib.Path`
+           * - private_key
+             - :py:class:`pathlib.Path`
+           * - processed_db_path
+             - :py:class:`pathlib.Path`
+           * - public_key
+             - :py:class:`pathlib.Path`
+           * - schema_registry_ssl_keystore_location
+             - :py:class:`pathlib.Path`
+           * - schema_registry_ssl_truststore_location
+             - :py:class:`pathlib.Path`
+           * - send_nsca_config
+             - :py:class:`pathlib.Path`
+           * - ssl_cacert
+             - :py:class:`pathlib.Path`
+           * - ssl_cert
+             - :py:class:`pathlib.Path`
+           * - ssl_certificate
+             - :py:class:`pathlib.Path`
+           * - ssl_certificate_path
+             - :py:class:`pathlib.Path`
+           * - ssl_key
+             - :py:class:`pathlib.Path`
+           * - ssl_keystore_location
+             - :py:class:`pathlib.Path`
+           * - ssl_keystore_path
+             - :py:class:`pathlib.Path`
+           * - ssl_truststore_location
+             - :py:class:`pathlib.Path`
+           * - ssl_truststore_path
+             - :py:class:`pathlib.Path`
+           * - statement_filepath
+             - :py:class:`pathlib.Path`
+           * - template
+             - :py:class:`pathlib.Path`
+           * - template_file
+             - :py:class:`pathlib.Path`
+           * - truststore
+             - :py:class:`pathlib.Path`
+        """
+        return self._ast.get_types()
+
     def parse(self) -> list:
+        """
+        Create an `Abstract syntax tree <https://en.wikipedia.org/wiki/Abstract_syntax_tree>`_  from the input data.
+        Of course, it is possible to parse all kinds of plugins, conditions and data types.
+
+        :return: a :py:class:`list`, the parsed tree
+
+        For example this input:
+
+        .. code-block:: python
+
+           from logstash_pipeline_parser import Pipeline
+
+           data = r\"""
+           input {
+             beats {
+               host => "0.0.0.0"
+               port => 5044
+               client_inactivity_timeout => 3600
+               include_codec_tag => true
+               enrich => [source_metadata, ssl_peer_metadata]
+               ssl => true
+               ssl_key => "/some/path/my.key"
+               id => "input_beats"
+             }
+           }
+           \"""
+
+           ast = Pipeline(data).parse()
+
+        will produce this array:
+
+        .. code-block:: python
+
+           from ipaddress import IPv4Address
+           from pathlib import Path
+
+           [
+               ["input",[
+                   ["beats", [
+                       ["host", [IPv4Address("0.0.0.0")]],
+                       ["port", [5044]],
+                       ["client_inactivity_timeout", [3600]],
+                       ["include_codec_tag", [True]],
+                       ["enrich", [
+                           ["source_metadata", "ssl_peer_metadata"]
+                       ]],
+                       ["ssl", [True]],
+                       ["ssl_key", [Path("/some/path/my.key")]],
+                       ["id", ["input_beats"]]
+                   ]]
+               ]]
+           ]
+        """
         return self._ast.parse_config(self._data)
 
-    def search(self, key: str) -> Generator[tuple, None, None]:
+    def search(self, key: str) -> Generator[tuple[str, Any], None, None]:
+        """
+        Yield the searched keys and their values from the tree.
+        The key can also contain the wildcard `*`, for example "output.*.hosts" will return
+        (if the pipeline definition contains them):
+
+        .. code-block:: console
+
+           - ("output.elasticsearch.hosts", ["127.0.0.1:9200","127.0.0.2:9200"])
+           - ("output.logstash.hosts", "127.0.0.1:9801")
+
+        .. note::
+
+           Please see :ref:`examples-search` for more examples.
+
+        :param key: a :py:class:`str`, the key name to search for
+        :return: a yield, the found values in the form tuple[key, value]
+        """
 
         for element in self._ast.parse_config(self._data):
             yield from Pipeline._recursive_search(key.split("."), element)
